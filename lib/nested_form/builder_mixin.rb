3ce386b2de54d89cff9364d6f865cb1fdd3dca12
pinglamb <pinglambs@gmail.com>
2014-12-11 19:02:37 +0800

    Support adding custom classes to the fields wrapper

--- lib/nested_form/builder_mixin.rb
+++ lib/nested_form/builder_mixin.rb
@@ -89,6 +89,7 @@ module NestedForm
 
     def fields_for_nested_model(name, object, options, block)
       classes = 'fields'
+      classes << ' ' + options[:wrapper_class] if options[:wrapper_class]
       classes << ' marked_for_destruction' if object.respond_to?(:marked_for_destruction?) && object.marked_for_destruction?
 
       perform_wrap   = options.fetch(:nested_wrapper, true)
